d9a70df9a23c53949524518a1eaccc396e165394
Xiaoyan Quan <quanx@usc.edu>
2026-04-19 04:24:01 -0700

    Fix AuthProvider remounting by moving it to createRoot level above RouterProvider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,12 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { Toaster } from "sonner";
-import { AuthProvider } from "./context/AuthContext";
-import { MatchProvider } from "./context/MatchContext";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <MatchProvider>
-        <RouterProvider router={router} />
-        <Toaster theme="dark" position="top-right" richColors />
-      </MatchProvider>
-    </AuthProvider>
+    <>
+      <RouterProvider router={router} />
+      <Toaster theme="dark" position="top-right" richColors />
+    </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
+  import { AuthProvider } from "./app/context/AuthContext.tsx";
+  import { MatchProvider } from "./app/context/MatchContext.tsx";
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+  createRoot(document.getElementById("root")!).render(
+    <AuthProvider>
+      <MatchProvider>
+        <App />
+      </MatchProvider>
+    </AuthProvider>
+  );
